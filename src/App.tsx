ad675aea58605ae70b4062a5140be1373aea8685
Ramon-Mateus <60753390+Ramon-Mateus@users.noreply.github.com>
2023-06-15 14:33:39 +0000

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css'
+import { useState, ChangeEvent } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setText(event.target.value);
+  };
+
   return (
     <main>
-      <p>Hello</p>
+      <div>
+        <input type="text" value={text} onChange={handleChange} />
+        <div>{text}</div>
+      </div>
     </main>
-  )
+  );
 }
